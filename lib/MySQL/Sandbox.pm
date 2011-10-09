e42842d92b7cc96e3b0264699ed0601d7c90f62d
g.maxia@gmail.com <>
2011-10-09 08:40:40 +0200

    - fixed CPAN Ticket 70470 MySQL::Sandbox - make_sandbox warns with Perl 5.14 and 5.14.1 - Fixed report-port in replication. The port used was the master's, but it

--- lib/MySQL/Sandbox.pm
+++ lib/MySQL/Sandbox.pm
@@ -23,7 +23,7 @@ our @EXPORT_OK= qw( is_port_open
                     sbinstr
                     get_option_file_contents ) ;
 
-our $VERSION="3.0.17";
+our $VERSION="3.0.18";
 our $DEBUG;
 
 BEGIN {
