82c22c72f8811b808c0681e7e061843580a93884
g.maxia@gmail.com <>
2011-12-14 11:34:56 +0100

    - fixed bug in prefixed version names (e.g.: now you can use make_sandbox ps5.1.57 or mp5.2.10) 	- Added arguments to deal with node options in replication and multiple sandbox. --node_options, --slave_options, --master_options, 	--one_slave_options, --one_node_options 	- added --high_performance option that adds the following features to the configuration file:         innodb-thread-concurrency=0         sync_binlog=0         innodb-log-buffer-size=50M         innodb-additional-mem-pool-size=100M         max-connections=350         max_allowed_packet=48M         innodb_buffer_pool_size=512M         innodb-log-file-size=50M         innodb-flush-method=O_DIRECT

--- lib/MySQL/Sandbox.pm
+++ lib/MySQL/Sandbox.pm
@@ -23,7 +23,7 @@ our @EXPORT_OK= qw( is_port_open
                     sbinstr
                     get_option_file_contents ) ;
 
-our $VERSION="3.0.21";
+our $VERSION="3.0.23";
 our $DEBUG;
 
 BEGIN {
